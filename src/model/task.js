41652988562074691faaf4ea84af0d645177ac41
Apple <riyabulia12@gmail.com>
2022-08-10 22:27:07 +0530

    Fixed eslint

--- src/model/task.js
+++ src/model/task.js
@@ -1,27 +1,27 @@
 class TASK {
-   constructor() {
-      this.taskList = [];
-   }
+  constructor() {
+    this.taskList = [];
+  }
 
-   static fetch() {
-      return JSON.parse(localStorage.getItem('task')) || [];
-   }
+  static fetch() {
+    return JSON.parse(localStorage.getItem('task')) || [];
+  }
 
-   static updateStorage(updatedTask) {
-      localStorage.setItem('task', JSON.stringify(updatedTask));
-   }
+  static updateStorage(updatedTask) {
+    localStorage.setItem('task', JSON.stringify(updatedTask));
+  }
 
-   add(task) {
-      this.taskList = TASK.fetch();
-      this.taskList.push(task);
-      TASK.updateStorage(this.taskList);
-   }
+  add(task) {
+    this.taskList = TASK.fetch();
+    this.taskList.push(task);
+    TASK.updateStorage(this.taskList);
+  }
 
-   updateIndex() {
-      this.taskList.forEach((item, id) => {
-         item.index = id + 1;
-      });
-   }
+  updateIndex() {
+    this.taskList.forEach((item, id) => {
+      item.index = id + 1;
+    });
+  }
 }
 
 export default TASK;
