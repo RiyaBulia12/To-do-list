ad643679a6e25913bb58105d99821033eb6385a9
Apple <riyabulia12@gmail.com>
2022-08-13 01:56:18 +0530

    Fixed linters

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './styles.css';
 import Tasks from './model/task.js';
-import { removeTaskUI, createTaskRow } from './crud';
-import { add } from 'lodash';
+import { removeTaskUI, createTaskRow } from './crud.js';
 
 const clear = document.querySelector('.clear');
 const addTask = document.getElementById('add-task');
@@ -13,63 +12,63 @@ let id = 0;
 
 task.taskList = Tasks.fetch();
 if (task.taskList) {
-   task.taskList.forEach((task, id) => {
-      createTaskRow(task.index, task.description);
-      // Add line-through for completed task when page is reload
-      if (task.completed) {
-         removeTaskUI(id + 1);
-      }
-   });
+  task.taskList.forEach((task, id) => {
+    createTaskRow(task.index, task.description);
+    // Add line-through for completed task when page is reload
+    if (task.completed) {
+      removeTaskUI(id + 1);
+    }
+  });
 }
-//localstorage for todo title
+// localstorage for todo title
 titleText.value = localStorage.getItem('title') ? JSON.parse(localStorage.getItem('title')) : 'To-Do List';
 
-const addTaskHelperMethod = () => {
-   if (addTask.value !== '') {
-      addTask.placeholder = "Press/click enter to add task";
-      addTask.classList.remove('empty-input');
-      id = task.taskList.length > 0 ? task.taskList[task.taskList.length - 1].index : 0;
-      id += 1;
-      const taskItem = { index: id, description: `${addTask.value}`, completed: false };
-      task.add(taskItem);
-      task.updateIndex();
-      createTaskRow(id, addTask.value);
-      addTask.value = '';
-      addTask.focus();
-   } else {
-      inputValidation();
-   }
+function inputValidation() {
+  addTask.placeholder = 'Empty Fields not allowed';
+  addTask.classList.add('empty-input');
 }
 
+const addTaskHelperMethod = () => {
+  if (addTask.value !== '') {
+    addTask.placeholder = 'Press/click enter to add task';
+    addTask.classList.remove('empty-input');
+    id = task.taskList.length > 0 ? task.taskList[task.taskList.length - 1].index : 0;
+    id += 1;
+    const taskItem = { index: id, description: `${addTask.value}`, completed: false };
+    task.add(taskItem);
+    task.updateIndex();
+    createTaskRow(id, addTask.value);
+    addTask.value = '';
+    addTask.focus();
+  } else {
+    inputValidation();
+  }
+};
+
 addTask.addEventListener('keypress', (event) => {
-   if (event.key === 'Enter') {
-      addTaskHelperMethod();
-   }
+  if (event.key === 'Enter') {
+    addTaskHelperMethod();
+  }
 });
 
 clear.addEventListener('click', () => {
-   task.taskList = Tasks.fetch();
-   task.taskList = task.taskList.filter((item) => !item.completed);
-   task.updateIndex();
-   Tasks.updateStorage(task.taskList);
-   window.location.reload();
+  task.taskList = Tasks.fetch();
+  task.taskList = task.taskList.filter((item) => !item.completed);
+  task.updateIndex();
+  Tasks.updateStorage(task.taskList);
+  window.location.reload();
 });
 
 titleText.addEventListener('keypress', (event) => {
-   if (event.key === 'Enter') {
-      localStorage.setItem('title', JSON.stringify(titleText.value));
-   };
+  if (event.key === 'Enter') {
+    localStorage.setItem('title', JSON.stringify(titleText.value));
+  }
 });
 
 refreshIcon.addEventListener('click', () => {
-   localStorage.setItem('title', JSON.stringify(titleText.value));
+  localStorage.setItem('title', JSON.stringify(titleText.value));
 });
 
 returnIcon.addEventListener('click', () => {
-   addTaskHelperMethod();
-})
-
-function inputValidation() {
-   addTask.placeholder = "Empty Fields not allowed";
-   addTask.classList.add('empty-input');
-}
+  addTaskHelperMethod();
+});
